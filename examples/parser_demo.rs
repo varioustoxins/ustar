ab14db31209d8d42bada047170f2b96acfbc4cb3
varioustoxins <garyt.and.sarahb@gmail.com>
2025-11-05 10:07:34 +0000

    add a simple demo of using the parser

---
+++ examples/parser_demo.rs
@@ -0,0 +1,34 @@
+use ustar::dump_extractors::dump_mutable_pair;
+use std::collections::HashMap;
+
+fn main() {
+    let input = r#"
+data_test
+    _double "hello world"
+    _single 'test string'
+    _non_quoted simple_value
+    _semicolon
+;
+Multi-line content
+with embedded text
+;
+"#;
+
+    println!("=== USTAR Parser Demo ===\n");
+    println!("Input:");
+    println!("{}", input);
+    println!();
+    
+
+    println!("\n=== Dump of Parsed Input ===\n");
+    let config_decompose = HashMap::new(); // Empty config defaults to true
+    
+    match ustar::parse(input, config_decompose) {
+        Ok(result) => {
+            dump_mutable_pair(&result, 0);
+        }
+        Err(e) => {
+            eprintln!("Parse error: {}", e);
+        }
+    }
+}
